7166fd1664a0786d020b92fb0a0b04a853a97451
Diana <maritimdiane96@gmail.com>
2023-02-08 13:04:18 +0300

    completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
-end
+    if array.length == 2
+      array.join(" and ")
+    elsif array.length > 2
+      str1 = array[0..array.length-2].join(", ")
+      str2 = array[-1].to_s
+      str1 + ", and " + str2
+    else
+      array.first.to_s
+    end
+  end
